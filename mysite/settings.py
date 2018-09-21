539246058de5a90bceb0a5a35eadaa81b1975f5c
karthic25 <klikesmath@yahoo.in>
2018-09-22 04:08:14 +0530

    collectstatic files deletion error resolved

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y3b(hgo#f&b9s((qr&dk+b7xu)=@g)9otg+#=e4m*r=bcyp28b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['karthic25.pythonanywhere.com']
+ALLOWED_HOSTS = ['karthic25.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
