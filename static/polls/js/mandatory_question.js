c99f95871aee715298d1e7c7538734cd6f8b3c6d
karthic25 <klikesmath@yahoo.in>
2018-09-22 05:31:45 +0530

    caching problem solved

--- static/polls/js/mandatory_question.js
+++ static/polls/js/mandatory_question.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $('h1').html("change3");
     $('#Vote').click(function() {
         var radioName = "choice";
         if ($('input[name=' + radioName + ']:checked').length) {
